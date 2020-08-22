31f6caf976cd37947b23268c152e903f0534ac90
ryuon <dododosa14@gmail.com>
2020-08-22 20:47:15 +0900

    add comments

--- models/Explorer/EpsilonGreedy/ConstantEpsilon.py
+++ models/Explorer/EpsilonGreedy/ConstantEpsilon.py
@@ -4,13 +4,27 @@ from models.Explorer.Explorer import Explorer
 
 class ConstantEpsilon(Explorer):
     """
+    イプシロンが定数値のε-greedy法
+    ε = const
 
+    Examples:
+        explorer = ConstantEpsilon(epsilon=0.2)
+        agent = Agent(explorer=explorer, args...)
     """
-    def __init__(self, epsilon=0.1):
+    def __init__(self, epsilon:float=0.1):
+        """
+        コンストラクタ
+
+        Args:
+            epsilon (float): 初期値
+        """
+
+        assert epsilon >= 0 and epsilon <= 1, "epsilon must be in [0, 1]"
+
         super(ConstantEpsilon, self).__init__()
         self.epsilon = epsilon
 
-    def explore(self, random_action_func:Callable[[], int], greedy_action_func:Callable[[], int]):
+    def explore(self, random_action_func:Callable[[], int], greedy_action_func:Callable[[], int]) -> int:
         if np.random.uniform(0, 1) <= self.epsilon:
             action = random_action_func()
         else:
@@ -19,4 +33,4 @@ class ConstantEpsilon(Explorer):
         return action
 
     def end_episode(self):
-        super().end_episode()
+        pass
--- models/Explorer/EpsilonGreedy/EpisodeLinearDecay.py
+++ models/Explorer/EpsilonGreedy/EpisodeLinearDecay.py
@@ -3,8 +3,30 @@ from typing import Callable
 from models.Explorer.Explorer import Explorer
 
 class EpisodeLinearDecay(Explorer):
+    """
+    直線的にεが減少していくε-greedy法
+
+    epsilon = - a * episode + start_epsilon (epsilon in [end_epsilon, start_epsilon])
+    
+    Examples:
+        explorer = EpisodeLinearDecay(decay_episodes=100, start_epsilon=0.7, end_epsilon=0.01)
+        agent = Agent(explorer=explorer, args...)
+    """
+
     def __init__(self, decay_episodes:int, start_epsilon:float=1.0, end_epsilon:float=0.0):
+        """
+        コンストラクタ
+
+        Args:
+            decay_episodes (int): start_epsilonからend_epsilonに至るまでにかかるエピソード数
+            start_episilon (float): εの初期値
+            end_epsilon (float): εが減少する最小値
+
+        """
         assert start_epsilon >= end_epsilon, "'start_epsilon' must be equal to or greater than 'end_epsilon'"
+
+        assert start_epsilon >= 0 and start_epsilon <= 1, "epsilon must be in [0, 1]"
+        assert end_epsilon >= 0 and end_epsilon <= 1, "epsilon must be in [0, 1]"
 
         super(EpisodeLinearDecay, self).__init__()
         self.epsilon = start_epsilon
@@ -26,6 +48,7 @@ class EpisodeLinearDecay(Explorer):
         self.episode += 1
 
         if self.episode < self.decay_episodes:
+            # 毎回直接もとめずに　(self.start_epsilon - self.end_epsilon) / self.decay_episodes　を引くだけでもいいかもしれない
             self.epsilon = self.start_epsilon - (self.start_epsilon - self.end_epsilon) / self.decay_episodes * self.episode
         else:
             self.epsilon = self.end_epsilon
